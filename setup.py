31f31a6b099fc209ed8555308f27ffe3a1f7448d
Duong Nhu <duong.nhu1@monash.edu>
2019-12-12 18:50:01 +1100

    v0.6

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@ import os
 import setuptools
 
 cwd = os.path.abspath(os.path.dirname(__file__))
-requirements = open(cwd + "/requirements.txt").read().split("\n")
+
+with open(cwd + "/requirements.txt") as f:
+    requirements = f.read().split("\n")
+
+with open(cwd + "/README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    version="0.1",
+    version="0.6",
     include_package_data=True,
     name="arxiv2bibpy",
     author="Duong Nhu",
     author_email="d.binhnhu@gmail.com",
     description="Search arxiv via api and save as bib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="arxiv, bibtext, bib",
     url="https://github.com/code-fury/arxiv2bib",
     packages=setuptools.find_packages(),
